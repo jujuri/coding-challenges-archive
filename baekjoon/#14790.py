748e2245e284cee7d73da9535ddb7cbd1be4024d
이주윤 <0612000ljy@naver.com>
2025-03-21 15:39:30 +0900

    Solve: #14790 - Tidy Numbers (Small) (Bronze 2)

---
+++ baekjoon/#14790.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    for j in range(n, 0, -1):
+        if str(j) == "".join(sorted(list(str(j)))):
+            print(f"Case #{i+1}: {j}")
+            break
+    
+
